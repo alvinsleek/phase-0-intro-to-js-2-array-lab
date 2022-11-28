e4b3724cabd95807b0715a8da030b1ecdf8a497d
Alvin Mithamo <alvinmithamo100@gmail.com>
2022-11-28 11:26:38 +0300

    Array lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push("Cole");
+function destructivelyAppendCat(){
+    cats.push("Cole")
+}
+
+function destructivelyPrependCat(name){
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(name){
+function destructivelyRemoveLastCat(){
+    cats.pop("Garfield")
+}
+
+function destructivelyRemoveFirstCat(name){
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo")
+}
+
+function removeFirstCat(Cole){
+    var newArray = cats.slice();
+    newArray.shift(Cole)
+    return newArray
+}
